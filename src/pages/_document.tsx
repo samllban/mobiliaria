d5d18493c99c372c16825cb438bad1cadc87c601
samllban <137834390+samllban@users.noreply.github.com>
2025-04-21 22:39:34 -0300

    remove licks header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-      <link rel="preload" href="/bg1.webp" as="image" />
-        <link rel="preload" href="/bg2.webp" as="image" />
-        <link rel="preload" href="/bg3.webp" as="image" />
-
       </Head>
       <body>
         <Main />
